8be1cfa07701644cec3ec041afd9925336caa594
HarshitSomal <hkaurece@gmail.com>
2016-02-23 10:10:10 -0800

    Initial commit

---
+++ app/src/main/java/com/example/robpercival/multipleactivities/SecondActivity.java
@@ -0,0 +1,55 @@
+package com.example.robpercival.multipleactivities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+
+    public void goBack(View view) {
+
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+
+        Intent i = getIntent();
+        textView2.setText("Hello " + i.getStringExtra("name"));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
